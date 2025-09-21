4c92a24e7b47df6711e001761137a116f21ff464
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-09-21 20:13:16 +0200

    Añadida la introducción de la historia de Sonic

--- src/components/pages/Sonic.jsx
+++ src/components/pages/Sonic.jsx
@@ -2,6 +2,25 @@ const Sonic = () => {
   return (
     <div id="container-race">
       <p>Historia de Sonic</p>
+      <p>Dodon Pa: Por fin llegó el día que todos esperaban, el CrossWorlds Grand Prix está a punto de comenzar y los corredores han descansado
+         lo suficiente para poder empezar su aventura de carrera ahora mismo. Vamos a ver como se prepara Sonic The Hedgehog para la carrera.<br/>
+         (Sonic prepara su coche y Extreme Gear)<br/>
+         Dodon Pa: Como te sientes, Sonic? Listo para empezar?<br/>
+         Sonic: Pues claro. Ardo en sueños de ganar esa competencia y llevarme el premio a casa.<br/>
+         Tails: Tan solo revisa tu Extreme Gear, vale Sonic? Siempre dejas la parte técnica a mí.<br/>
+         Sonic: No te preocupes, ganaré esa competencia con los ojos cerrados.<br/>
+         Knuckles: Venga, podemos empezar ya? Mi cuerpo rebosa de fuerza listo para correr. Venga, empecemos ya!<br/>
+         Dodon Pa: Ya veo que estás animado. Preparate, porque tú primer Grand Prix será contra Shadow.<br/>
+      </p>
+      <p>
+        Shadow: No me esperaba que nos enfrentaramos a la primera de cambio, me servirás como calentamiento. Pero recuerda que yo, la forma
+        de vida definitiva ganará esa competencia.<br/>
+        Sonic: Je, eso ya lo veremos, Shadow<br/>
+        Tails: Shadow y su Dark Reaper tiene velocidad mejorada que la última vez, te recomendaría que fueras a la personalización antes de empezar
+        la carrera. También recuerda que necesitas equiparte un set de artilugios que te pueden ayudar a dar la vuelta a la tortilla<br/>
+        Sonic: Perfecto! Me prepararé y le daré una buena a Shadow!
+      </p>
+
     </div>
   )
 }
