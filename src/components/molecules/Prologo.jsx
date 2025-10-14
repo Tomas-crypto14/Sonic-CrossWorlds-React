8e8cea9862a5f0e0a6af089f2a7b38c2ddcacff4
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-10-14 09:48:35 +0200

    Prólogo actualizado

--- src/components/molecules/Prologo.jsx
+++ src/components/molecules/Prologo.jsx
@@ -4,11 +4,12 @@ const Prologo = () => {
   return (
     <div id="container-race">
         <h1>Prologo</h1>
-        <p>DodonPa: Por fin ha llegado el momnento que todos estaban esperando, la competición CrossWorlds Grand Prix empezará cuando todos los corredores
+        <p>DodonPa: Por fin ha llegado el momento que todos estaban esperando, la competición CrossWorlds Grand Prix empezará cuando todos los corredores
            se hayan reunido aquí, en el planeta de Donpa. 
         </p>
         <p>(Tails, Knuckles y Amy llegan a la pista)</p>
-        <p>Knuckles: Hola, Tails, cuanto tiempo. Veo que te has preparado a fondo.<br/>
+        <p>
+            Knuckles: Hola, Tails, cuanto tiempo. Veo que te has preparado a fondo.<br/>
             Tails: Así es, Knuckles. Me he apuntado a esta competición para que veais los resultados de mi viaje después de 
             aquella experiencia en Starfall Islands<br/>
             Amy: Yo al enterarme de la invitación de DodonPa he usado mi coche para repasar mi conducción y aún mejor, mi Extreme Gear
@@ -18,23 +19,27 @@ const Prologo = () => {
         Knuckles: Parece que aún no ha llegado
         </p>
         <p>(Llegan Silver y Blaze)</p>
-        <p>Silver: Hola, chicos. Como os va?<br/>
-        Blaze: Habeis sido los primeros en llegar?<br/>
-        (Amy, Knuckles y Tails asienten)<br/>
-        Knuckles: Veo que vuestros vehículos son mejores que la otra vez.</p>
+        <p>
+            Silver: Hola, chicos. Como os va?<br/>
+            Blaze: Habeis sido los primeros en llegar?<br/>
+            (Amy, Knuckles y Tails asienten)<br/>
+            Knuckles: Veo que vuestros vehículos son mejores que la otra vez.
+        </p>
         (Llegan Cream y Big)<br/>
-        <p>Cream: Hola, chicos. Me alegro mucho de volver a veros<br/>
+        <p>
+            Cream: Hola, chicos. Me alegro mucho de volver a veros<br/>
             Amy: Hola, Cream. Como me alegro de verte.<br/>
             Big: Bien, el equipo Rose de vuelta(Se alegra)<br/>
         </p>
-        <p>Jet: Jajajajajaja! Por favor!<br/>
-        (Llega el equipo Babylon)<br/>
-        Jet: Nosotros, el Team Babylon no nos andamos con chiquitas en esta competencia, ya veréis que nuestros Extreme Gears lo bordarán todo<br/>
-        Tails: Jet, Wave, Storm!? Ha pasado mucho tiempo!<br/>
-        Wave: Has mejorado tu ingeniería la última vez que nos vimos?<br/>
-        Tails: Esta vez no seré el que pierda!<br/>
-        Storm: Cuanto tiempo sin vernos, Equidna Rojo, listo para la paliza de revancha?<br/>
-        Knuckles: Esta vez tú perderás, saco de huesos.
+        <p>
+            Jet: Jajajajajaja! Por favor!<br/>
+            (Llega el equipo Babylon)<br/>
+            Jet: Nosotros, el Team Babylon no nos andamos con chiquitas en esta competencia, ya veréis que nuestros Extreme Gears lo bordarán todo<br/>
+            Tails: Jet, Wave, Storm!? Ha pasado mucho tiempo!<br/>
+            Wave: Has mejorado tu ingeniería la última vez que nos vimos?<br/>
+            Tails: Esta vez no seré el que pierda!<br/>
+            Storm: Cuanto tiempo sin vernos, Equidna Rojo, listo para la paliza de revancha?<br/>
+            Knuckles: Esta vez tú perderás, saco de huesos.
         </p>
 
         <p>
@@ -45,7 +50,7 @@ const Prologo = () => {
            Knuckles: Obvio que esos tres siempre serán así, no han cambiado nada. 
         </p>
         <p>
-            Cream: El sr.Sonic ya debería estar aquí, que raro.<br/>
+            Cream: El Sr.Sonic ya debería estar aquí, que raro.<br/>
             (El Team Dark(Shadow, Rouge y Omega) está en pie)<br/>
             Knuckles: Eh, Shadow. Sonic ha llegado?<br/>
             Shadow: Hola, Knuckles. No, Sonic aún no ha llegado<br/>
@@ -71,7 +76,7 @@ const Prologo = () => {
         <p>
             (El Dr.Eggman llega junto a Metal Sonic, Sage y un EggPawn)<br/>
             Dr.Eggman: Ah, rayos. Llegamos tarde, Sonic ha llegado antes que nosotros.<br/>
-            Sonic: Nah, da igual, yo habría dicho lo mimsmo que tú si habrías llegado antes que yo, por lo cual eso no importa.<br/>
+            Sonic: Nah, da igual, yo habría dicho lo mismo que tú si habrías llegado antes que yo, por lo cual eso no importa.<br/>
             Dr.Eggman: Ay, mira que eres.<br/>
             Sage(Mirando a Sonic, Tails, Knuckles y Amy): Me alegro mucho de veros, he venido para ayudar a mi padre a disfrutar
             de la competencia, con más de un millón de simulaciones en el que tendrá posibilidad de derrotaros.<br/>
