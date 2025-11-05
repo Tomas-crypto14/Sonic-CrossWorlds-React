9a704a9ddc60fa466dd54df33e3aedd28badc355
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-11-05 11:38:59 +0100

    Historia completa y actualizada

--- src/components/pages/Rouge-Story/Rouge2.jsx
+++ src/components/pages/Rouge-Story/Rouge2.jsx
@@ -8,6 +8,48 @@ const Rouge2 = () => {
     <div id="container-race">
         <h2>Historia de Rouge</h2>
         <h3>Velocidad Sonic</h3>
+
+        <p>
+          Cream: Hola, Sra.Rouge. Vas a competir en el Wisp Grand Prix después de esta carrera?<br/>
+          Rouge: Vaya, vaya, lindos mis ojos, es que vas a correr luwgo contra mí?<br/>
+          Cream: Pues claro, además, en el Colorful Mall hay artículos lujosos que seguro que competiremos por ellos.<br/>
+          Rouge: Entiendo, el que pierda, paga, eh? Vale, acepto el reto.
+        </p>
+
+        <p>
+          (Al ganar el Wisp Grand prix)
+        </p>
+
+        <p>
+          Rouge: Jejejeje! He ganado!<br/>
+          Cream: Oh, no! Perdí!<br/>
+          Rouge: Tranquila, ya pagaré luego todo, no te preocupes.<br/>
+          Cream: No es por eso, Rouge. Tan solo me lo he pasado bien corriendo contra ti.<br/>
+          Rouge: Ayyy, niñas de hoy en día.
+        </p>
+
+        <p>
+          (Al llegar al DeathEgg Grand Prix)
+        </p>
+
+        <p>
+          Rouge: Me pregunto contra quién me enfrentaré ahora.<br/>
+          Jet: Te encontré, maldita ladrona murciélago!<br/>
+          Rouge: Anda, si eres el jefe de aquella banda de ladrones, los Babylon Rogues.<br/>
+          Jet: Vas a pagarme de todo lo que has robado, maldita ladrona, robaste la joya de Soleanna,
+          el zafiro de Adabat y nuestra relliquia, te vas a enterar!<br/>
+          Rouge: Ah, era tuyo? Cuánto lo siento, bueno, vamos a correr, no?
+        </p>
+
+        <p>
+          (Al ganar el DeathEgg Grand Prix)
+        </p>
+
+        <p>
+          Jet: Maldita mocosa! No me puedo creer que haya perdido contra esa murcielago!<br/>
+          Wave: Dejalo, Jet. Ya tendremos posibilidades de ganarla.<br/>
+          Rouge: Uff, cada vez que robo algo que querían ellos anteriormente, me encuentro en problemas.
+        </p>
         
         <HeaderRouge/>
         <Routes>
--- src/components/pages/Shadow-Story/Shadow2.jsx
+++ src/components/pages/Shadow-Story/Shadow2.jsx
@@ -10,6 +10,75 @@ const Shadow2 = () => {
         <h2>Historia de Shadow</h2>
         <h3>Velocidad Sonic</h3>
 
+        <p>
+          Shadow: Nueva clase, nuevos competidores que dan la talla para derrotarme.<br/>
+          Knuckles: Hola, Shadow. Adivina quien ha conseguido el Donpa Grand Prix antes que tú.<br/>
+          Shadow: Tú, no?<br/>
+          Knuckles: Exacto, ya verás cuando llegue al Coral Grand Prix y te dé una paliza.<br/>
+          Shadow: Je, ya lo veremos.
+        </p>
+
+        <p>
+          (Al llegar el Coral Grand Prix)
+        </p>
+
+        <p>
+          Knuckles: Vaya, por fin llega "la forms de vida definitiva". Preparado?<br/>
+          Shadow: Hum. Aprenderás a aceptar mi superioridad y sobre todo respetar a mis superiores.
+        </p>
+
+        <p>
+          (Al ganar el Coral Grand Prix)
+        </p>
+
+        <p>
+          Knuckles: No me lo puedo creer, he perdido.<br/>
+          Shadow: Ha sido más difícil de lo que yo pensaba, ha sido una buena carrera después de todo.<br/>
+          Knuckles: Sí, ya te digo.
+        </p>
+
+        <p>
+          (Al llegar al Crystal Grand Prix)
+        </p>
+
+        <p>
+          Shadow: Me enfrento a la hija del doctor?<br/>
+          Sage: Ya que mi padre ha ganado ese Grand Prix y ha llegado a la última para clasificarse,
+          debo encargarme de que nadie le gane, además, me encantaría comprobar tus capacidades de velocidad.<br/>
+          Shadow: Lo que quieres es correr contra mñi, no? Perfecto, hacía tiempo que quería luchar contra ti,
+          muéstrame esa fuerza de inteligencia artifical de la que tanto hablaban Sonic y el doctor.
+        </p>
+
+        <p>
+          (Al ganar el Crystal Grand Prix)
+        </p>
+
+        <p>
+          Sage: Las expectativas sobre ti han sido sobrepasadas, eres la froma de vida definitiva 
+          después de todo<br/>
+          Shadow: Siempre gano y siempre lo consigo. Bueno, me voy a enfrentar al doctor.
+        </p>
+
+        <p>
+          (Al llegar al DeathEgg Grand Prix)
+        </p>
+
+
+        <p>
+          Shadow: Hola, doctor.<br/>
+          Eggman: Shadow?! Así que Sage ha perdido, eh? Bueno, hizo lo mejor que pudo, lo hizo bien.<br/>
+          Shadow: Qué raro, Eggman regañaría a Sage por haber perdido contra mí, qué le habrá pasado?
+        </p>
+
+        <p>
+          (Al ganar el DeathEgg Grand Prix)
+        </p>
+
+        <p>
+          Dodon Pa: Y el erizo oscuro pasa a la siguiente clase.<br/>
+          Shadow: Jum! Muy pronto, me enfrentaré a Sonic y ganaré esta competencia.
+        </p>
+
         <HeaderShadow/>
             <Routes>
                 <Route path="/Shadow" element={<Shadow/>}/>
