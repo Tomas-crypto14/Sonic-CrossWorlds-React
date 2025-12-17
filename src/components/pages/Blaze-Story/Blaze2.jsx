5135b5da3d00d6e30bf14474ef616bf601cd6fc4
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-12-17 15:08:47 +0100

    Historia terminada

--- src/components/pages/Blaze-Story/Blaze2.jsx
+++ src/components/pages/Blaze-Story/Blaze2.jsx
@@ -9,6 +9,47 @@ const Blaze2 = () => {
         <h2>Historia de Blaze</h2>
         <h3>Velocidad Sonic</h3>
 
+        <p>
+          (Al ganar el Calabaza Grand Prix)
+        </p>
+
+        <p>
+          Blaze: Bien, cuatro copas conseguidas.<br/>
+          Sonic: Hola, Blaze! Sorpendida de verme?<br/>
+          Blaze: Eh, Sonic! Me alegro de verte, como va por aquí?<br/>
+          Sonic: Ah, nada, diciendote que me enfrentaré a ti en el Coral Grand Prix, te veo en la línea de meta.<br/>
+          Blaze: Je, ese Sonic.
+        </p>
+
+        <p>
+          (Al ganar el Coral Grand Prix)
+        </p>
+
+        <p>
+          Sonic: He... perdido? Vaya, quizá si eres mejor y muy buena corredora como yo.<br/>
+          Blaze: Sonic.<br/>
+          Sonic: Te voy a decir algo, si alguna vez Eggman o Zavok planean hacer de las suyas en tu mundo, estaré aquí para detenerlos.
+          Puedes contar con ello.
+        </p>
+
+        <p>
+          (Al llegar al DeathEgg Grand Prix)
+        </p>
+
+        <p>
+          Silver: Estoy listo para enfrentarme a ti, Blaze!<br/>
+          Blaze: Vaya, ahora cara a cara contra Silver? Eso me gusta más.
+        </p>
+
+        <p>
+          (Al ganar el DeathEgg Grand Prix)
+        </p>
+
+        <p>
+          Silver: No me lo puedo creer, pero fue buena carrera, Blaze! Suerte en la siguiente clase!<br/>
+          Blaze: Gracias!
+        </p>
+
         <HeaderBlaze/>
         <Routes>
             <Route path="/Blaze" element={<Blaze/>}/>
--- src/components/pages/Silver-Story/Silver2.jsx
+++ src/components/pages/Silver-Story/Silver2.jsx
@@ -9,6 +9,45 @@ const Silver2 = () => {
         <h2>Historia de Silver</h2>
         <h3>Velocidad Sonic</h3>
 
+        <p>
+          (Al ganar el Wisp Grand Prix)
+        </p>
+
+        <p>
+          Silver: Tanta competencia con esos trazados demuestra que Dodon Pa ha creado buena tecnología para estas simulaciones,
+          pero tengo una duda. Porque no usó lugares que tengan que ver con mi futuro? Cuando algo pasa mal en el presente de Sonic,
+          mi futuro está en ruinas, habrá planeado eso antes de que otro desastre se avecinaba en mi futuro? No, eso sería imposible.
+        </p>
+
+        <p>
+          (Al llegar al Coral Grand Prix)
+        </p>
+
+        <p>
+          Silver: Imposible, me tengo que enfrentar ahora a Metal Sonic?<br/>
+          Metal Sonic: ...!<br/>
+          Silver: No me queda más remedio que derrotarte.
+        </p>
+
+        <p>
+          (Al ganar el Coral Grand Prix)
+        </p>
+
+        <p>
+          Silver: Toma ya! Se siente, Metal!<br/>
+          Metal Sonic: ...<br/>
+          Shadow: Si que ha dolido, para ser una chatarra.
+        </p>
+
+        <p>
+          (Al llegar al DeathEgg Grand Prix)
+        </p>
+
+        <p>
+          Shadow: Hola, Silver, me alegro de verte.<br/>
+          Silver: Shadow! Así que tú eres mi próximo oponente!<br/>
+          Shadow: Así es, y esta vez no pienso andarme con chiquitas.
+        </p>
         <HeaderSilver/>
         <Routes>
             <Route path="/Silver" element={<Silver/>}/>
